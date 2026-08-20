3ca975270a663b9e5ae822a6a5fccfde4df2bb1e
Apoorv Shukla <apoorvshukla072@gmail.com>
2026-08-20 20:30:01 +0530

    Added Controller

---
+++ src/main/java/com/ftf/transaction_service/controller/TransactionController.java
@@ -0,0 +1,43 @@
+package com.ftf.transaction_service.controller;
+
+import com.ftf.transaction_service.client.AccountServiceClient;
+import com.ftf.transaction_service.client.dto.AccountResponse;
+import com.ftf.transaction_service.dto.TransactionRequest;
+import com.ftf.transaction_service.dto.TransactionResponse;
+import com.ftf.transaction_service.service.TransactionService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/transactions")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+    private final AccountServiceClient accountServiceClient;
+    public TransactionController(TransactionService transactionService, AccountServiceClient accountServiceClient) {
+        this.transactionService = transactionService;
+        this.accountServiceClient=accountServiceClient;
+    }
+
+    @PostMapping
+    public ResponseEntity<TransactionResponse> createTransaction(
+            @Valid @RequestBody TransactionRequest request) {
+
+        TransactionResponse response =
+                transactionService.createTransaction(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+    @GetMapping("/test-account/{accountNumber}")
+    public ResponseEntity<AccountResponse> testAccount(
+            @PathVariable String accountNumber) {
+
+        return ResponseEntity.ok(
+                accountServiceClient.getAccountByNumber(accountNumber)
+        );
+    }
+}
