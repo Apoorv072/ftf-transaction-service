75ff638d19c6f1c1ba0a512a2688e9680803986d
Apoorv Shukla <apoorvshukla072@gmail.com>
2026-08-20 20:24:52 +0530

    Added Acoount Service (Client) and RestTemplate Configurations

---
+++ src/main/java/com/ftf/transaction_service/client/AccountServiceClient.java
@@ -0,0 +1,27 @@
+package com.ftf.transaction_service.client;
+
+import com.ftf.transaction_service.client.dto.AccountResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class AccountServiceClient {
+
+    private final RestTemplate restTemplate;
+
+    public AccountServiceClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public AccountResponse getAccountByNumber(String accountNumber) {
+
+               String url =
+                "http://localhost:8081/api/v1/accounts/byNumber/"+accountNumber;
+
+        return restTemplate.getForObject(
+                url,
+                AccountResponse.class,
+                accountNumber
+        );
+    }
+}
---
+++ src/main/java/com/ftf/transaction_service/client/dto/AccountResponse.java
@@ -0,0 +1,17 @@
+package com.ftf.transaction_service.client.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class AccountResponse {
+
+    private Long id;
+    private String accountNumber;
+    private String currency;
+    private BigDecimal balance;
+    private String status;
+
+    // getters and setters
+}
